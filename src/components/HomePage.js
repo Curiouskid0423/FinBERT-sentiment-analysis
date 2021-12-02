7e47f6314cb8b30b66e67498bc44747f1225435d
Yu-Teng Li <yutengli@berkeley.edu>
2021-12-01 23:15:49 -0800

    prototype done

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import { 
     Box, 
@@ -9,46 +9,69 @@ import {
     GridItem, 
     Input, 
     InputGroup, 
+    FormControl, 
+    InputLeftAddon, 
 } from '@chakra-ui/react';
 import { CheckCircleIcon } from '@chakra-ui/icons';
 
 import Portfolio from "./Portfolio";
 
 export default function HomePage() {
-  return (
-    <Box textAlign="center" py={10} px={6} m={"3rem"}>
-      <CheckCircleIcon boxSize={'50px'} color={"blue.500"} />
-      <Heading as="h2" size="xl" mt={6} mb={2}>
-        Financial News Headline <Text as={'span'} color={"blue.500"}>Sentiment Analysis</Text> Demo
-      </Heading>
-      <Grid p={9}>
-        <GridItem>
-            <Text color={'gray.500'} fontSize={"1.2rem"}>
-                Your Portfolio
-            </Text>
-        </GridItem>
-        <GridItem py={2}>
-            <Grid
-                templateRows='repeat(1, 1fr)'
-                templateColumns='repeat(5, 1fr)'
-                gap={4}
-            >
-                <GridItem colSpan={4}>
-                    <InputGroup>
-                        <Input placeholder='Search' />
-                    </InputGroup>
-                </GridItem>
-                <GridItem colSpan={1}>
-                    <Button variant={"solid"} bg={"blue.300"} color={"white"} colorScheme={"blue"}>
-                        Add Stock
-                    </Button>
-                </GridItem>
-            </Grid>
-        </GridItem>
-        <GridItem py={2}>
-            <Portfolio />
-        </GridItem>
-      </Grid>
-    </Box>
+
+    const [headlineState, handleHeadlineState] = useState()
+
+    const onCheckHeadline = (e) => {
+        console.log(e.target.value);
+    }
+
+    return (
+        <Box textAlign="center" py={10} px={6} m={"3rem"}>
+        <CheckCircleIcon boxSize={'50px'} color={"blue.500"} />
+        <Heading as="h2" size="xl" mt={6} mb={2}>
+            Financial News Headline <Text as={'span'} color={"blue.500"}>Sentiment Analysis</Text> Demo
+        </Heading>
+        <Grid p={9}>
+            <GridItem>
+                <Text color={'gray.500'} fontSize={"1.2rem"}>
+                    Your Portfolio
+                </Text>
+            </GridItem>
+            <GridItem py={2}>
+                <Grid>
+                    <GridItem colSpan={5}>
+                        <form onSubmit={onCheckHeadline}>
+                        <FormControl id='headlineSearch'>
+                            <Grid 
+                                templateRows='repeat(1, 1fr)'
+                                templateColumns='repeat(5, 1fr)'
+                                gap={4}
+                            >
+                                <GridItem colSpan={4}>
+                                    <InputGroup>
+                                        <InputLeftAddon children="Headline"/>    
+                                        <Input 
+                                            placeholder={"e.g. BlackRock threatens buyers strike for Illinois debt"} 
+                                            name={"headline"} onChange={handleHeadlineState}
+                                        />
+                                    </InputGroup>
+                                </GridItem>
+                                <GridItem colSpan={1}>
+                                    <Button 
+                                        variant={"solid"} colorScheme={"blue"} 
+                                        bg={"blue.300"} type={"submit"}>
+                                        Submit
+                                    </Button>
+                                </GridItem>
+                            </Grid>
+                        </FormControl>
+                        </form>
+                    </GridItem>
+                </Grid>
+            </GridItem>
+            <GridItem py={2}>
+                <Portfolio />
+            </GridItem>
+        </Grid>
+        </Box>
   );
 }
